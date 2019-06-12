92068e5a95133aa50723f532fecfc2d8b91aa2b0
Patrick Keating <keating916@gmail.com>
2019-06-11 20:57:12 -0700

    add history

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let display = document.getElementById("display");
+let output = document.getElementById("output")
 let disallowLetters = /[a-z]/gi
 let disallowSymbols = /[\!\@\#\$\%\%\^\&\(\)\_\|\<\>\?\,]/g
 let all = /[\*\/\+\-]/g
@@ -6,11 +7,7 @@ let m = /\*/
 let d = /\//
 let a = /\+/
 let s = /\-/
-display.addEventListener("keydown", e => {
-    if(e.keyCode < 48 || e.keycode >57) {
-        e.preventDefault;
-    }
-})
+
 for(let i = 0; i < 16; i++) {
     //add event listeners to each button
     let t = display.value.length;
@@ -20,15 +17,15 @@ for(let i = 0; i < 16; i++) {
             changeDisplay(button)
         }else{
             console.log(typeof display.value)
-            let str = display.value
+            let str = display.value;
             let t1 = disallowLetters.test(str)
             let t2 = disallowSymbols.test(str)
             if(t1 != false || t2 != false){
-                console.log("I'm Sorry, only numbers and basic functions allowed!")
                 clear()
+                display.value = "I'm Sorry, only numbers and basic functions allowed!"
             }else{
-            let i1 = getOps(display.value).join("")
-            let i2 = display.value.split(all)
+            let i1 = getOps(str).join("")
+            let i2 = str.split(all)
             multiOps(i1,i2)
             }
         }
@@ -38,6 +35,7 @@ for(let i = 0; i < 16; i++) {
 function multiOps(arr1, arr2) {
     //arr1 is the array of operators
     //arr2 is the array of numbers entered
+    let str = display.value;
     let times = arr1.length
     for(let i = 0; i < times; i++) { //iterate over how many operations were used
         if(arr1.search(m) != -1) { //search for multiplication first
@@ -67,6 +65,7 @@ function multiOps(arr1, arr2) {
         }
     }
     console.log(typeof arr2[0]);
+    addHistory(str, arr2[0])
     display.value = arr2[0] //display final product
 }
 
@@ -79,6 +78,11 @@ function changeDisplay(b) {
     }else{
         display.value = display.value + b.value;
     }
+}
+function addHistory(input, answer) {
+    let out = document.createElement("p")
+    out.textContent = `${input} = ${answer}`
+    output.appendChild(out);
 }
 
 function clear() {
